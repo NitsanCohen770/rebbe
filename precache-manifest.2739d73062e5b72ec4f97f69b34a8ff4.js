a115dba9d00bc84d7c5bf367ffb9c4080251d914
NitsanCohen770 <nitsancohen770@gmail.com>
2021-02-28 15:56:25 +0200

    Updates

---
+++ precache-manifest.2739d73062e5b72ec4f97f69b34a8ff4.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2768714319133d7a6958eb4167c8f2a6",
+    "url": "/rebbe/index.html"
+  },
+  {
+    "revision": "1efefce6528739fa0019",
+    "url": "/rebbe/static/css/2.0dc148fc.chunk.css"
+  },
+  {
+    "revision": "7383049fce536ac26e59",
+    "url": "/rebbe/static/css/main.1d837444.chunk.css"
+  },
+  {
+    "revision": "1efefce6528739fa0019",
+    "url": "/rebbe/static/js/2.6cded1ed.chunk.js"
+  },
+  {
+    "revision": "fb54723782ed10b07cad473d3ad54f4f",
+    "url": "/rebbe/static/js/2.6cded1ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7383049fce536ac26e59",
+    "url": "/rebbe/static/js/main.faf6a7b7.chunk.js"
+  },
+  {
+    "revision": "7084cca948295c7cd5ec",
+    "url": "/rebbe/static/js/runtime-main.7ec78e9b.js"
+  },
+  {
+    "revision": "fe8bed5feea747e0797e24c4e8b26bb2",
+    "url": "/rebbe/static/media/404.fe8bed5f.png"
+  },
+  {
+    "revision": "05fccaecdf145a831aa1712e2f3e65c0",
+    "url": "/rebbe/static/media/about.05fccaec.jpg"
+  },
+  {
+    "revision": "939d78a0842e638839c6216da3646d48",
+    "url": "/rebbe/static/media/blog-hero.939d78a0.png"
+  },
+  {
+    "revision": "4a8c832994b48ee4bfc599baaae4410e",
+    "url": "/rebbe/static/media/blog-single.4a8c8329.jpg"
+  },
+  {
+    "revision": "9c9a750812bab056b475fb9cc078f51e",
+    "url": "/rebbe/static/media/favicon.9c9a7508.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rebbe/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rebbe/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rebbe/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rebbe/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rebbe/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0a8f0dff9d80807d7c29bb251e6dce5b",
+    "url": "/rebbe/static/media/hero-bg.0a8f0dff.jpg"
+  },
+  {
+    "revision": "66047f0c4f8516827a24fdc1ecda0c2f",
+    "url": "/rebbe/static/media/logo.66047f0c.png"
+  },
+  {
+    "revision": "63954f40cc8c6b5a14bc15f723d695d0",
+    "url": "/rebbe/static/media/portfolio-1.63954f40.jpg"
+  },
+  {
+    "revision": "9117866351e8d4f164ca1ee6fc42e985",
+    "url": "/rebbe/static/media/portfolio-2.91178663.jpg"
+  },
+  {
+    "revision": "63954f40cc8c6b5a14bc15f723d695d0",
+    "url": "/rebbe/static/media/portfolio-long-1.63954f40.jpg"
+  },
+  {
+    "revision": "9117866351e8d4f164ca1ee6fc42e985",
+    "url": "/rebbe/static/media/portfolio-long-2.91178663.jpg"
+  },
+  {
+    "revision": "9795fef0d8ee77a8aaef78ca9d677602",
+    "url": "/rebbe/static/media/portfolio-long-3.9795fef0.jpg"
+  },
+  {
+    "revision": "b7717a69aba21beb03bfa039bc70d1db",
+    "url": "/rebbe/static/media/portfolio-long-4.b7717a69.jpg"
+  },
+  {
+    "revision": "8f4edce027f6539520f54909562c48ca",
+    "url": "/rebbe/static/media/portfolio-long-5.8f4edce0.jpg"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/rebbe/static/media/react.84287d09.svg"
+  },
+  {
+    "revision": "53da834cf982da90647dc32565f82abf",
+    "url": "/rebbe/static/media/service-bg.53da834c.png"
+  },
+  {
+    "revision": "34c6010a2a87fcd287f2680335bc516a",
+    "url": "/rebbe/static/media/slider-1.34c6010a.jpg"
+  },
+  {
+    "revision": "aca54e4d62bf00e7ef808e315e33ff03",
+    "url": "/rebbe/static/media/slider-bg.aca54e4d.jpg"
+  }
+]);
